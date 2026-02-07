30196b3b84d46fc15048ab6bdc55087f80fc6aa2
Luis <fasterplop@gmail.com>
2026-02-07 13:02:19 -0400

    small changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,22 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fortitudeins.us', // Dominio final necesario para sitemap/canonical
+  // Dominio final necesario para sitemap/canonical
+  site: 'https://fortitudeins.us',
+
   integrations: [react(), tailwind()],
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
     routing: {
         prefixDefaultLocale: false // / para inglés, /es/ para español
     }
-  }
+  },
+
+  adapter: cloudflare()
 });
